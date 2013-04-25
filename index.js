281f6a84b34e1144ebf8529846fa8d221c1ae325
Max <max@teamatb.com>
2013-04-25 14:07:18 -0500

    Has routes to get and serve forecast.

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
+var util = require('util')
+
+var events = require('events')
+var e = new events.EventEmitter()
+
+var Forecast = require('forecast.io')
+forecast = new Forecast({APIKey: process.env.FORECAST_API_KEY})
 
 app.use(express.bodyParser())
 app.use(express.static(__dirname + '/public'))
 app.use(express.logger())
 
-app.get('/', function(req,res) {res.send('game.html')})
+app.get('/', function(req,res) {res.sendfile('index.html')})
+app.get('/bundle.js', function(req,res) {res.sendfile('bundle.js')})
 
-app.get('/forecast', getForecast(req,res))
+app.get('/forecast', function(req,res) {
+	e.emit('get weather', {lat: req.query.latitude, lon: req.query.longitude } )
+	e.on('weather data', function(weather) {res.send(weather)})
+})
 
 
-function getForecast(req,res) {}
+e.on('get weather', getForecast)
+function getForecast(coord) {
+	forecast.get(coord.lat, coord.lon, function (err, res, data) {
+		if (err) throw err
+		e.emit('weather data', data.hourly)
+	});
+}
+
+server.listen(3000);
+console.log('Listening on port 3000')
